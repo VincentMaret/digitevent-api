372b610e444ebe926de23b73ddfe2a14fce4efa9
Vincent Maret <vincent.maret@verallia.com>
2019-09-30 10:11:16 +0200

    init project

--- dist/index.js
+++ dist/index.js
@@ -2,15 +2,18 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _cors = _interopRequireDefault(require("cors"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
+app.use((0, _cors["default"])());
 app.set('port', process.env.PORT || 3000); //----------------------------------------------
 //------------SET ROUTS-------------------------
 //----------------------------------------------
 
 app.get('/', function (req, res, next) {
-  res.send('hello worldddssd');
+  res.send('hello world');
 }); //----------------------------------------------
 //------------Launch Server---------------------
 //----------------------------------------------
